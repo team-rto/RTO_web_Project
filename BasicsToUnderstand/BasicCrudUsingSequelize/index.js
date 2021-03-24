6cc601a9a865162e108124e6dfeca180a9cc1dee
VasisthaPrakhar <work.prakharvasistha@gmail.com>
2021-03-24 18:49:37 +0530

    Basic Crud

--- BasicsToUnderstand/BasicCrudUsingSequelize/index.js
+++ BasicsToUnderstand/BasicCrudUsingSequelize/index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const path = require('path');
+const ejsMate =  require('ejs-mate');
+const { Sequelize } = require('sequelize');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+
 const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+
 
 const db = require("./models");
 
@@ -20,6 +35,30 @@ app.get('/select', (req, res) => {
         console.log(err);
     });
 });
+app.get('/home', catchAsync(async (req, res) => {
+    const campgrounds = await User.findAll();
+    res.render('campgrounds/index', { campgrounds })
+}))
+
+app.get('/home/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+app.post('/new', catchAsync(async (req, res, next) => {
+    //if (!req.body.campground) throw new ExpressError('Invalid Campground Data!!', 400);
+   
+    const user = new User(req.body.User);
+    const u = user.dataValues;
+    await User.create(u);
+
+    res.redirect(`/home/${user._id}`)
+}))
+app.get('/home/:id', catchAsync(async (req, res) => {
+    console.log(req.params.id);
+    const campground1 = await User.findAll({ where: {id: req.params.id}});
+    const campground = campground1[0].dataValues;
+    res.render('campgrounds/show', {campground});
+}))
+
 
 app.get('/insert', (req, res) => {
     User.create({
--- BasicsToUnderstand/BasicCrudUsingSequelize/models/User.js
+++ BasicsToUnderstand/BasicCrudUsingSequelize/models/User.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define("User", {
-        firstName:{
+        Name:{
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
@@ -13,7 +13,36 @@ module.exports = (sequelize, DataTypes) => {
             validate: {
                 notEmpty: true
             }
-        }
+        },
+        Dob:{
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        father:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        Category:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        Address:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+
     });
     return User;
 };
