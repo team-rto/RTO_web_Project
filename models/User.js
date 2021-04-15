5132f302ccaf11158471168f678de5f00e5c033e
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-04-15 16:42:01 +0530

    Added Login/Register button in navbar

--- models/User.js
+++ models/User.js
@@ -1,48 +1,48 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define("User", {
-        Name:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        age:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        Dob:{
-            type: DataTypes.DATEONLY,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        father:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        Category:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        Address:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-
-    });
-    return User;
+  const User = sequelize.define("User", {
+    Name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    Dob: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    father: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    Category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    Address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  });
+  return User;
 };
+
