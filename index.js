9762f2e02fb7c65222984258e60ba721077f2fe0
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-04-20 22:55:27 +0530

    changes made to host on heroku, line 19 in index.js is commented, do check

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(session({ secret: "notagoodsecret" }));
+// app.use(session({ secret: "notagoodsecret" })); // heroku was showing error.
 app.use(express.static("public"));
 
 const db = require("./models");
